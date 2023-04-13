6d9e43162ec64321e07357efbb36928b43e1b354
Naddalyy <nathalie.suckfiel@googlemail.com>
2023-04-13 14:45:12 +0200

    Added new entries in the glossary

--- Chapter 6 (Dictionaries)/glossary.py
+++ Chapter 6 (Dictionaries)/glossary.py
@@ -8,7 +8,15 @@ glossary = {
     'for loop': 'iterates through each item in a list/set/dictionary/..',
     'dictionary': 'contains key-value pairs, much like an actual dictionary',
     'list': 'can contain multiple items that are mutable',
-    'catchall statement': 'else block - matches any condition that is not matched by if or elif test'
+    'catchall statement': 'else block - matches any condition that is not matched by if or elif test',
+    'function': 'named blocks of code designed to perform specific actions',
+    'function call': 'execute code that is in the function',
+    'modules': 'seperate files that help organizing main program files',
+    'parameter': 'piece of information needed in functions',
+    'argument': 'piece of information that is passed from a function call into a function',
+    'positional arguments': 'need to be in the same order as the parameters',
+    'keyword arguments': 'each argument consists of a name-value pair',
+    'default values in functions': 'can be assigned for parameters, so argument in function call can be excluded',
     }
 
 print("Glossary:")
