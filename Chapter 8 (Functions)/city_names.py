365ec2219a884a328b8188caec344b8010a8cf74
Naddalyy <nathalie.suckfiel@googlemail.com>
2023-04-20 17:06:28 +0200

    Added exercise (8-6. City Names)

---
+++ Chapter 8 (Functions)/city_names.py
@@ -0,0 +1,6 @@
+def city_country(name, country):
+    return (f"{name.title()}, {country.title()}")
+   
+print(city_country('amsterdam', 'the netherlands'))
+print(city_country('paris', 'france'))
+print(city_country('düsseldorf', 'germany'))
