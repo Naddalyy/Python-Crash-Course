6b681e35e691a8486d41f64a58510501b340294c
Naddalyy <nathalie.suckfiel@googlemail.com>
2023-04-20 17:49:01 +0200

    Added exercise (8-9. Messages)

---
+++ Chapter 8 (Functions)/messages.py
@@ -0,0 +1,6 @@
+def send_messages(messages):
+    for message in messages:
+        print(message)
+
+short_messages = ["I am learning python!", "I like pancakes.", "Hi there!", ":)"]
+send_messages(short_messages)
