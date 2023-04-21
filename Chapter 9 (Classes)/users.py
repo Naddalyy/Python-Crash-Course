695e381b9bec2c7542a0c0630fb6830b83c0933c
Naddalyy <nathalie.suckfiel@googlemail.com>
2023-04-21 22:01:26 +0200

    Updated file with exercise (9-5. Login Attempts)

--- Chapter 9 (Classes)/users.py
+++ Chapter 9 (Classes)/users.py
@@ -6,19 +6,29 @@ class User:
         self.date_of_birth = date_of_birth
         self.username = username
         self.location = location.title()
+        self.login_attempts = 0
 
     def describe_user(self):
         """Displaying a summary of the user's information."""
         full_name = f"{self.first_name} {self.last_name}"
         print(full_name)
         print(f" Username: {self.username}")
-        print(f" Date of birth: {self.date_of_birth}")
+        print(f" Date of Birth: {self.date_of_birth}")
         print(f" Location: {self.location}")
 
     def greet_user(self):
         """Greeting the user."""
         msg = f"Welcome back, {self.username}!\n"
         print(msg)
+
+    def increment_login_attempts(self):
+        """Increments the value of login_attempts by 1."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Sets the value of login_attempts to 0."""
+        self.login_attempts = 0
+
 
 # Creating instances of the class and calling its methods
 user_one = User("jenny", "miller", "23.04.2002", "J.Miller", "United States")
@@ -32,3 +42,13 @@ user_two.greet_user()
 user_three = User("kyo", "sohma", "15.01.1983", "KyoCat", "Japan")
 user_three.describe_user()
 user_three.greet_user()
+
+
+# 9-5. Login Attempts
+user_three.increment_login_attempts()
+user_three.increment_login_attempts()
+user_three.increment_login_attempts()
+user_three.increment_login_attempts()
+print(f"{user_three.username} tried to log in {user_three.login_attempts} times.")
+user_three.reset_login_attempts()
+print(f"After reseting - Login Attemps: {user_three.login_attempts}")
